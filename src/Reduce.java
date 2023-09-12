9fc6bf1c2cf208ff08b5ce7e5397e592f948e406
sungju <sungju@hanseongjuui-MacBookPro.local>
2023-09-11 22:10:05 -0400

    Reduce function

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    static int n = 0;
+    public static void main(String[] args) {
+        for (int i = 100; i >0; i-- ){
+           if (i % 2 == 0){
+               i = i/2;
+           }
+           else {
+               i = i - 1;
+           }
+            n++;
+        }
+        System.out.println(n);
+    }
 }
